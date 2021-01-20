350bc8f0ce7ff1ebc232cb84b6e64ce5ada9610a
ekatynski <edkatynski@gmail.com>
2021-01-20 16:35:20 -0500

    Added configFileCheck function and config defaults

--- src/main/java/com/hatci/ccs/Configurator.java
+++ src/main/java/com/hatci/ccs/Configurator.java
@@ -1,16 +1,28 @@
 package com.hatci.ccs;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Configurator {
 
-    // ensures all regular use directories are present
+    // default configuration file values
+    private int col_Can = 8;
+    private int col_category = 15;
+    private int col_feature = 3;
+    private int col_test_case = 14;
+    private int col_US = 7;
+    private String format = ".xslx";
+    private boolean include_invalid = false;
+    private boolean include_other = false;
+    private int leading_sheets = 2;
+    private int row_start = 17;
+
     public Configurator() {
+        // ensures all regular use directories are present
         this.directoryCheck("config");  // contains config.json file
         this.directoryCheck("input");   // where checksheet files are placed
         this.directoryCheck("output");  // where the comparison chart is saved
+
+        this.configFileCheck();
     }
 
     public void directoryCheck(String dirName) {
@@ -25,5 +37,31 @@ public class Configurator {
                         + dirName.substring(1) + " directory created.");
             }
         }
+    }
+
+    public void configFileCheck() {
+        // check if config file exists
+        File configFile = new File("../config/config.json");
+        // create config file if none exists
+        if (!configFile.isFile()) {
+            try {
+                System.out.println("No config file detected.");
+                configFile.createNewFile();
+                if (configFile.isFile()) {
+                    System.out.println("Config file created.");
+                }
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void configFileSet() {
+
+    }
+
+    public void configFileRead() {
+
     }
 }
