793dc0c51dd9bac9393c4e4aaad09788ea6650b1
ekatynski <edkatynski@gmail.com>
2021-01-20 17:25:30 -0500

    Developed config file setup method

--- src/main/java/com/hatci/ccs/Configurator.java
+++ src/main/java/com/hatci/ccs/Configurator.java
@@ -1,6 +1,7 @@
 package com.hatci.ccs;
 
 import java.io.File;
+import java.io.FileWriter;
 
 public class Configurator {
 
@@ -14,6 +15,9 @@ public class Configurator {
     private boolean include_invalid = false;
     private boolean include_other = false;
     private int leading_sheets = 2;
+    private int platform_col = 3;
+    private int platform_row = 3;
+    private int platform_sheet = leading_sheets + 1;
     private int row_start = 17;
 
     public Configurator() {
@@ -41,7 +45,8 @@ public class Configurator {
 
     public void configFileCheck() {
         // check if config file exists
-        File configFile = new File("../config/config.json");
+        String fileName = "../config/config.json";
+        File configFile = new File(fileName);
         // create config file if none exists
         if (!configFile.isFile()) {
             try {
@@ -49,6 +54,7 @@ public class Configurator {
                 configFile.createNewFile();
                 if (configFile.isFile()) {
                     System.out.println("Config file created.");
+                    this.configFileSet(fileName);
                 }
             }
             catch(Exception e) {
@@ -57,11 +63,36 @@ public class Configurator {
         }
     }
 
-    public void configFileSet() {
-
+    public void configFileSet(String fileName) {
+        try {
+            FileWriter configFile = new FileWriter(fileName);
+            // export default config settings to config file
+            configFile.write("{\n" +
+                    " \"col_Can\": 8,\n" +
+                    " \"col_category\": 8,\n" +
+                    " \"col_feature\": 15,\n" +
+                    " \"col_test_case\": 14,\n" +
+                    " \"col_US\": 7,\n" +
+                    " \"format\": \".xlsx\",\n" +
+                    " \"include_invalid\": false,\n" +
+                    " \"include_other\": false,\n" +
+                    " \"leading_sheets\": 2,\n" +
+                    " \"platform\": {\n" +
+                    "\t\"col\": 3,\n" +
+                    "\t\"row\": 3,\n" +
+                    "\t\"sheet\": 3\n" +
+                    " },\n" +
+                    " \"row_start\": 17\n" +
+                    "}");
+            configFile.close();
+            System.out.println("Config settings successfully updated.");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void configFileRead() {
-
+        
     }
 }
--- src/test/java/com/hatci/ccs/AppTest.java
+++ src/test/java/com/hatci/ccs/AppTest.java
@@ -1,8 +1,5 @@
 package com.hatci.ccs;
 
-import java.io.File;
-import java.io.FileInputStream;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
 public class AppTest {
