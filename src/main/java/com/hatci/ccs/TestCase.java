2fdfa9b129a098b3118ccecfbc879cb3246c44c3
ekatynski <edkatynski@gmail.com>
2021-01-26 17:57:38 -0500

    Staging for complete program rebuild.

--- src/main/java/com/hatci/ccs/TestCase.java
+++ src/main/java/com/hatci/ccs/TestCase.java
@@ -2,7 +2,12 @@ package com.hatci.ccs;
 
 public class TestCase {
 
-
+    private String caseNumber;
+    private String featureName;
+    private String title;
+    private String usResult;
+    private String canResult;
+    private boolean isInvalid;
 
     TestCase() {
 
